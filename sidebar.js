f39428c6381bb212c09aa62aa22d2ef5b9bed0ba
Kostas <kostasspanolios@gmail.com>
2022-06-17 12:34:34 +0100

    add menu functionality, transition effect

---
+++ sidebar.js
@@ -0,0 +1,22 @@
+openMenu = () => {
+  document.querySelector(".backdrop").classList = "backdrop active";
+  document.querySelector("aside").classList = "active";
+};
+
+closeMenu = () => {
+  document.querySelector(".backdrop").classList = "backdrop";
+  document.querySelector("aside").classList = "";
+};
+
+document.getElementById("menuBtn").onclick = (e) => {
+  e.preventDefault();
+  openMenu();
+};
+
+document.querySelector("aside button.close-button").onclick = () => {
+  closeMenu();
+};
+
+document.querySelector(".backdrop").onclick = () => {
+  closeMenu();
+};
